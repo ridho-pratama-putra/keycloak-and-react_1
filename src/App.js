b99d957d285aef97cde8673acc7bfb2b548c7bb8
ridho <ridho.putra@btpn.com>
2022-01-18 09:50:47 +0700

    wrap App inside of keycloak

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import {ReactKeycloakProvider} from "@react-keycloak/web";
+import React from "react";
+import keycloak from './configs/keycloak'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <ReactKeycloakProvider authClient={keycloak}>
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        onClick={keycloak.login}
+                    >
+                        Learn React
+                    </a>
+                </header>
+            </div>
+        </ReactKeycloakProvider>
+    );
 }
 
 export default App;
---
+++ src/configs/keycloak.js
@@ -0,0 +1,4 @@
+import Keycloak from 'keycloak-js'
+
+const keycloak = new Keycloak('/keycloak.json');
+export default keycloak
