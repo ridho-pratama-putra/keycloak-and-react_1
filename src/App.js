e5270126dabdd15cf2d85f3f2f7fcb028e30c553
ridho <ridho.putra@btpn.com>
2022-01-20 11:12:37 +0700

    beautify

--- src/App.js
+++ src/App.js
@@ -56,34 +56,14 @@ function App() {
                         <img style={{width: '40px', height: 'auto'}} src={logo} className="App-logo" alt="logo"/>
 
                         <Box sx={{flexGrow: 1, display: {xs: 'flex', md: 'none'}}}>
-                            <IconButton
-                                size="large"
-                                aria-label="account of current user"
-                                aria-controls="menu-appbar"
-                                aria-haspopup="true"
-                                onClick={handleOpenNavMenu}
-                                color="inherit"
-                            >
+                            <IconButton size="large" aria-label="account of current user" aria-controls="menu-appbar"
+                                        aria-haspopup="true" onClick={handleOpenNavMenu} color="inherit">
                                 <MenuIcon/>
                             </IconButton>
-                            <Menu
-                                id="menu-appbar"
-                                anchorEl={anchorElNav}
-                                anchorOrigin={{
-                                    vertical: 'bottom',
-                                    horizontal: 'left',
-                                }}
-                                keepMounted
-                                transformOrigin={{
-                                    vertical: 'top',
-                                    horizontal: 'left',
-                                }}
-                                open={Boolean(anchorElNav)}
-                                onClose={handleCloseNavMenu}
-                                sx={{
-                                    display: {xs: 'block', md: 'none'},
-                                }}
-                            >
+                            <Menu id="menu-appbar" anchorEl={anchorElNav}
+                                  anchorOrigin={{vertical: 'bottom', horizontal: 'left',}} keepMounted
+                                  transformOrigin={{vertical: 'top', horizontal: 'left',}} open={Boolean(anchorElNav)}
+                                  onClose={handleCloseNavMenu} sx={{display: {xs: 'block', md: 'none'},}}>
                                 <BrowserRouter>
                                     {pages.map((page) => (
                                         <MenuItem key={page} onClick={handleCloseNavMenu} component={Link}
@@ -94,24 +74,13 @@ function App() {
                                 </BrowserRouter>
                             </Menu>
                         </Box>
-                        <Typography
-                            variant="h6"
-                            noWrap
-                            component="div"
-                            sx={{flexGrow: 1, display: {xs: 'flex', md: 'none'}}}
-                        >
-                            LOGO
-                        </Typography>
+                        <Typography variant="h6" noWrap component="div"
+                                    sx={{flexGrow: 1, display: {xs: 'flex', md: 'none'}}}> LOGO </Typography>
                         <Box sx={{flexGrow: 1, display: {xs: 'none', md: 'flex'}}}>
                             <BrowserRouter>
                                 {pages.map((page) => (
-                                    <Button component={Link}
-                                            to={`/${page === 'Home' ? '' : page.toLocaleLowerCase()}`}
-                                            key={page}
-                                            sx={{my: 2, color: 'white', display: 'block'}}
-                                    >
-                                        {page}
-                                    </Button>
+                                    <Button component={Link} to={`/${page === 'Home' ? '' : page.toLocaleLowerCase()}`}
+                                            key={page} sx={{my: 2, color: 'white', display: 'block'}}>{page}</Button>
                                 ))}
                             </BrowserRouter>
                         </Box>
@@ -122,30 +91,17 @@ function App() {
                                     <Avatar alt="Remy Sharp" src="/static/images/avatar/image path.jpg"/>
                                 </IconButton>
                             </Tooltip>
-                            <Menu
-                                sx={{mt: '45px'}}
-                                id="menu-appbar"
-                                anchorEl={anchorElUser}
-                                anchorOrigin={{
-                                    vertical: 'top',
-                                    horizontal: 'right',
-                                }}
-                                keepMounted
-                                transformOrigin={{
-                                    vertical: 'top',
-                                    horizontal: 'right',
-                                }}
-                                open={Boolean(anchorElUser)}
-                                onClose={handleCloseUserMenu}
-                            >
+                            <Menu sx={{mt: '45px'}} id="menu-appbar" anchorEl={anchorElUser}
+                                  anchorOrigin={{vertical: 'top', horizontal: 'right',}} keepMounted
+                                  transformOrigin={{vertical: 'top', horizontal: 'right',}} open={Boolean(anchorElUser)}
+                                  onClose={handleCloseUserMenu}>
                                 <BrowserRouter>
-
                                     {settings.map((setting) => {
-                                        if (setting === 'Logout') {
-                                            return (<MenuItem key={setting} onClick={handleCloseNavMenu}>
-                                                <Typography textAlign="center">{setting}</Typography>
-                                            </MenuItem>)
-                                        }
+                                            if (setting === 'Logout') {
+                                                return (<MenuItem key={setting} onClick={handleCloseNavMenu}>
+                                                    <Typography textAlign="center">{setting}</Typography>
+                                                </MenuItem>)
+                                            }
                                             return (<MenuItem key={setting} component={Link}
                                                               to={`/${setting.toLocaleLowerCase()}`}>
                                                 <Typography textAlign="center">{setting}</Typography>
