99339d25fc82a70fca16473cda1b7150d5c3fe5e
ridho <ridho.putra@btpn.com>
2022-01-29 00:19:05 +0700

    replace backend URL to match with nginx

--- src/api/Blog.js
+++ src/api/Blog.js
@@ -1,4 +1,4 @@
 import instance from '../configs/Interceptors';
 
-const BLOG_URL = 'http://localhost:8082/sec';
+const BLOG_URL = 'http://localhost:8899/api-1/sec';
 export const getBlog = () => instance.get(BLOG_URL, {timeout: 5000});
