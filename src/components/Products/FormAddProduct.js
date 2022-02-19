0c10826c9d432b9909d18ef3e1c17ed9671307d0
ridho <ridho.putra@btpn.com>
2022-02-19 16:15:28 +0700

    sample table result

--- src/components/Products/FormAddProduct.js
+++ src/components/Products/FormAddProduct.js
@@ -6,7 +6,7 @@ import FormGroup from "@mui/material/FormGroup";
 import Button from "@mui/material/Button";
 import {useDispatch} from "react-redux";
 import {createProduct} from "../../action/Product";
-import FormControl from "@mui/material/FormControl";
+import Card from "@mui/material/Card";
 
 const FormAddProduct = () => {
     const referenceForms = useRef();
@@ -37,93 +37,92 @@ const FormAddProduct = () => {
 
     return (
         <div>
-            Form add product
-            <Box sx={{
-                marginTop: '30px',
-                border: '1px solid grey',
-                borderRadius: '5px',
-                padding: '15px 20px 15px 20px',
-            }}>
-                <ValidatorForm
-                    ref={referenceForms}
-                    component='form'
-                    onSubmit={handleSubmit}
-                >
-                    <FormGroup>
-                        <div style={{
-                            display: 'flex',
-                            justifyContent: 'space-around',
-                        }}>
-                            <TextValidator
-                                fullWidth
-                                label='Description*'
-                                validators={['required']}
-                                errorMessages={['required']}
-                                onChange={(event) => setDescription(event.target.value)}
-                                value={description}
-                                sx={{
-                                    width: '50ch',
-                                    marginTop: '10px',
-                                }}
-                            />
-                            <TextValidator
-                                label='Sell price*'
-                                validators={['required', 'minNumber:0']}
-                                errorMessages={['required', 'minimum 0']}
-                                InputProps={{
-                                    inputComponent: PriceInput,
-                                }}
-                                onChange={(event) => setSellPrice(event.target.value)}
-                                value={sellPrice}
-                                sx={{
-                                    width: '50ch',
-                                    marginTop: '10px',
-                                }}
-                            />
-                        </div>
-                        <div style={{
-                            display: 'flex',
-                            justifyContent: 'space-around'
-                        }}>
-                            <TextValidator
-                                label='Barcode*'
-                                validators={['required']}
-                                errorMessages={['required']}
-                                onChange={(event) => setBarcode(event.target.value)}
-                                value={barcode}
-                                sx={{
-                                    marginTop: '10px',
-                                    width: '50ch',
+            <Card variant='outlined'>
+                <Box sx={{
+                    marginTop: '30px',
+                    padding: '15px 20px 15px 20px',
+                }}>
+                    <ValidatorForm
+                        ref={referenceForms}
+                        component='form'
+                        onSubmit={handleSubmit}
+                    >
+                        <FormGroup>
+                            <div style={{
+                                display: 'flex',
+                                justifyContent: 'space-around',
+                            }}>
+                                <TextValidator
+                                    fullWidth
+                                    label='Description*'
+                                    validators={['required']}
+                                    errorMessages={['required']}
+                                    onChange={(event) => setDescription(event.target.value)}
+                                    value={description}
+                                    sx={{
+                                        width: '50ch',
+                                        marginTop: '10px',
+                                    }}
+                                />
+                                <TextValidator
+                                    label='Sell price*'
+                                    validators={['required', 'minNumber:0']}
+                                    errorMessages={['required', 'minimum 0']}
+                                    InputProps={{
+                                        inputComponent: PriceInput,
+                                    }}
+                                    onChange={(event) => setSellPrice(event.target.value)}
+                                    value={sellPrice}
+                                    sx={{
+                                        width: '50ch',
+                                        marginTop: '10px',
+                                    }}
+                                />
+                            </div>
+                            <div style={{
+                                display: 'flex',
+                                justifyContent: 'space-around'
+                            }}>
+                                <TextValidator
+                                    label='Barcode*'
+                                    validators={['required']}
+                                    errorMessages={['required']}
+                                    onChange={(event) => setBarcode(event.target.value)}
+                                    value={barcode}
+                                    sx={{
+                                        marginTop: '10px',
+                                        width: '50ch',
 
-                                }}
+                                    }}
 
-                            />
-                            <TextValidator
-                                label='Stock Available*'
-                                validators={['required']}
-                                errorMessages={['required']}
-                                onChange={(event) => setStockAvailable(event.target.value)}
-                                value={stockAvailable}
-                                sx={{
-                                    marginTop: '10px',
-                                    width: '50ch',
+                                />
+                                <TextValidator
+                                    label='Stock Available*'
+                                    validators={['required']}
+                                    errorMessages={['required']}
+                                    onChange={(event) => setStockAvailable(event.target.value)}
+                                    value={stockAvailable}
+                                    sx={{
+                                        marginTop: '10px',
+                                        width: '50ch',
 
-                                }}
+                                    }}
 
-                            />
-                        </div>
-                        <div style={{
-                            display: 'flex',
-                            justifyContent: 'flex-end',
-                        }}>
-                            <Button variant='contained' onClick={() => clearForm()}
-                                    sx={{margin: '20px 15px 10px 15px'}}>Reset form</Button>
-                            <Button variant='contained' type='submit'
-                                    sx={{margin: '20px 15px 10px 15px'}}>Submit</Button>
-                        </div>
-                    </FormGroup>
-                </ValidatorForm>
-            </Box>
+                                />
+                            </div>
+                            <div style={{
+                                display: 'flex',
+                                justifyContent: 'flex-end',
+                            }}>
+                                <Button variant='contained' onClick={() => clearForm()}
+                                        sx={{margin: '20px 15px 10px 15px'}}>Reset form</Button>
+                                <Button variant='contained' type='submit'
+                                        sx={{margin: '20px 15px 10px 15px'}}>Submit</Button>
+                            </div>
+                        </FormGroup>
+                    </ValidatorForm>
+                </Box>
+            </Card>
         </div>
     );
 };
--- src/components/Products/ListProduct.js
+++ src/components/Products/ListProduct.js
@@ -1,11 +1,117 @@
-import React, {PropTypes} from 'react';
+import React from 'react';
 import Box from "@mui/material/Box";
+import NumberFormat from 'react-number-format';
+import Card from "@mui/material/Card";
+import MUIDataTable from 'mui-datatables';
+// import { ThemeProvider} from '@mui/styles';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 const ListProduct = (props) => {
+
+    const columns = [
+        {
+            name: 'name',
+            label: 'Name',
+            options: {
+                filter: true,
+                sort: true
+            }
+        },
+        {
+            name: 'buyPrice',
+            label: 'Buy Price',
+            options: {
+                sort: true,
+                customBodyRender: (value, tableMeta, updateValue) => (
+                    <NumberFormat
+                        displayType={'text'}
+                        value={value}
+                        thousandSeparator={true}
+                        isNumericString={true}
+                        prefix={'Rp. '}
+                    />
+                )
+            }
+        },
+        {
+            name: 'offlineSellPrice',
+            label: 'Offline Price',
+            options: {
+                sort: true,
+                customBodyRender: (value, tableMeta, updateValue) => (
+                    <NumberFormat
+                        displayType={'text'}
+                        value={value}
+                        thousandSeparator={true}
+                        isNumericString={true}
+                        prefix={'Rp. '}
+                    />
+                )
+            }
+        },
+        {
+            name: 'onlineSellPrice',
+            label: 'Online Price',
+            options: {
+                sort: true,
+                customBodyRender: (value, tableMeta, updateValue) => (
+                    <NumberFormat
+                        displayType={'text'}
+                        value={value}
+                        thousandSeparator={true}
+                        isNumericString={true}
+                        prefix={'Rp. '}
+                    />
+                )
+            }
+        },
+        {
+            name: 'productQuantity',
+            label: 'Quantity',
+            options: {
+                sort: true
+            }
+        }
+    ];
+
+    const getMuiTheme = () =>
+        createTheme({
+            overrides: {
+                MUIDataTable: {
+                    paper: {
+                        boxShadow: 'none'
+                    }
+                }
+            }
+        });
+
+    const dataForTable = [{
+        productQuantity: 4,
+        name: 'stock',
+        buyPrice: '50000',
+        offlineSellPrice: '400000',
+        onlineSellPrice: '3000'
+    }];
+
+
     return (
         <div>
-            <Box sx={{border: '1px solid black'}}>
-                List Product
+            <Box sx={{
+                marginTop: '15px'
+            }}>
+                <Card variant='outlined'>
+                    <ThemeProvider theme={getMuiTheme()}>
+                        <MUIDataTable
+                            title={'Product List'}
+                            data={dataForTable}
+                            columns={columns}
+                            options={{
+                                selectableRows: 'none' // <===== will turn off checkboxes in rows
+                            }}
+                        />
+                    </ThemeProvider>
+                </Card>
+
             </Box>
         </div>
     );
